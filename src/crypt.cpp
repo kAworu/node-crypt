d95b4c3b676f8f05beac69941efc1a4430d5480e
Alexandre Perrin <alex@kaworu.ch>
2014-03-17 09:48:10 +0100

    simpler string conversion from v8 to C.

--- src/crypt.cpp
+++ src/crypt.cpp
@@ -8,21 +8,10 @@ using namespace v8;
 Handle<Value> CryptHash(const Arguments& args) {
 	HandleScope scope;
 
-	if (args.Length() < 2) {
-		ThrowException(Exception::TypeError(String::New("Wrong number of arguments")));
-		return scope.Close(Undefined());
-	}
+	String::Utf8Value key(args[0]->ToString());
+	String::Utf8Value salt(args[1]->ToString());
 
-	Local<String> key  = args[0]->ToString();
-	Local<String> salt = args[1]->ToString();
-
-	char *c_key  = new char[key->Length() + 1];
-	char *c_salt = new char[salt->Length() + 1];
-	key->WriteAscii(c_key);
-	salt->WriteAscii(c_salt);
-	char *hash = crypt(c_key, c_salt);
-	delete[] c_key;
-	delete[] c_salt;
+	char *hash = crypt(*key, *salt);
 
 	return scope.Close(hash ? String::New(hash) : Undefined());
 }
